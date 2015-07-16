f4ea73f308371ee5d2ed8f2d0eb0980cd4c9668a
dafabulousteach <dafabulousteach@gmail.com>
2015-07-15 19:55:23 -0700

    PlayerView passing all tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,14 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
+	//initalize method to start the methods
   initialize: function(){
+  	//using the var that to bind the state of 'this'
   	var that = this;
+  	//listening for the 'add' method
 	this.on('add',function(){
+		//Checking the queue for the purpose of autoplay
 		if(this.length < 2){
+
 			this.playFirst();
 		} 
 	}),
@@ -15,6 +20,7 @@ var SongQueue = Songs.extend({
 	}),
 	this.on('dequeue', function(){
 		this.remove(this.at(0));
+		this.playFirst();
 	})
 
   },
--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,6 +6,11 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    this.render();
+    var that = this;
+    this.$el.on('ended', function(){
+      that.model.dequeue();
+    })
   },
 
   setSong: function(song){
