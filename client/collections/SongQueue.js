2eea84879fa3baab3e39c79fcf5b3055e867fdca
dafabulousteach <dafabulousteach@gmail.com>
2015-07-15 18:57:30 -0700

    SongQueue tests pass

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -5,15 +5,22 @@ var SongQueue = Songs.extend({
 	this.on('add',function(){
 		if(this.length < 2){
 			this.playFirst();
-		}
+		} 
 	}),
 	this.on('ended', function(){
 		this.remove(this.at(0))
+		if(this.length > 0){
+			this.playFirst();
+		} 
+	}),
+	this.on('dequeue', function(){
+		this.remove(this.at(0));
 	})
 
   },
 
   playFirst: function(){
-  		
+  	this.at(0).play();
+  	
   }
 });
