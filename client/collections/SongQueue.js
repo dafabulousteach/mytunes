c609de85ffcff397d88083113e822b9a76bdde4b
dafabulousteach <dafabulousteach@gmail.com>
2015-07-15 17:36:11 -0700

    SongQueue is passing all but the playsFirst thing.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,12 +1,19 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
-
   initialize: function(){
-  	this.playFirst();
+  	var that = this;
+	this.on('add',function(){
+		if(this.length < 2){
+			this.playFirst();
+		}
+	}),
+	this.on('ended', function(){
+		this.remove(this.at(0))
+	})
+
   },
 
   playFirst: function(){
-
+  		
   }
-
 });
